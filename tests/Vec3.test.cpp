81a9ea13065ec75946f05157949ae11b59b3053a
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-16 22:03:01 +0300

    test: add a first test using Catch2
    - This is just to confirm that everything is working as intended

--- tests/Vec3.test.cpp
+++ tests/Vec3.test.cpp
@@ -1,6 +1,14 @@
-#include <cstdlib>
+#include <catch2/catch_test_macros.hpp>
 
-int main()
+constexpr unsigned int factorial(unsigned number) noexcept
 {
-    return EXIT_SUCCESS;
+    return number <= 1 ? number : factorial(number - 1) * number;
+}
+
+TEST_CASE("Factorials are computed", "[factorial]")
+{
+    REQUIRE(factorial(1) == 1);
+    REQUIRE(factorial(2) == 2);
+    REQUIRE(factorial(3) == 6);
+    REQUIRE(factorial(10) == 3628800);
 }
