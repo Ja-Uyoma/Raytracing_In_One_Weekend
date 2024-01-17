26e130d30c926808be213145cef10b15cc3fcbb0
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-17 16:53:47 +0300

    test: add tests for accessors
    - We test the x(), y(), and z() methods, as well as the
    operator[] overloads

--- tests/Vec3.test.cpp
+++ tests/Vec3.test.cpp
@@ -24,3 +24,29 @@ TEST_CASE("Vectors are constructed", "[Vec3]")
         REQUIRE(vec[2] == 3);
     }
 }
+
+TEST_CASE("Accessors work as intended", "[Vec3]")
+{
+    constexpr auto vec = Vec3(1, 2, 3);
+
+    SECTION("x() gets the x-coordinate only")
+    {
+        REQUIRE(vec.x() == vec[0]);
+        REQUIRE(vec.x() != vec[1]);
+        REQUIRE(vec.x() != vec[2]);
+    }
+
+    SECTION("y() gets the y-coordinate only")
+    {
+        REQUIRE(vec.y() == vec[1]);
+        REQUIRE(vec.y() != vec[0]);
+        REQUIRE(vec.y() != vec[2]);
+    }
+
+    SECTION("z() gets the z-coordinate only")
+    {
+        REQUIRE(vec.z() == vec[2]);
+        REQUIRE(vec.z() != vec[0]);
+        REQUIRE(vec.z() != vec[1]);
+    }
+}
