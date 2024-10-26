a2f72a8b64d8fdfc195db0c6f5cbb0391e4203df
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-26 17:29:16 +0300

    refactor: move function rayHasHitSphere to Main.hpp

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -32,40 +32,6 @@
 
 namespace rt {
 
-/// \brief Determine if a ray has hit the sphere in the viewport
-/// \param[in] centre The centre of the sphere
-/// \param[in] radius The radius of the sphere
-/// \param[in] ray The ray under test
-/// \returns True if the ray has hit the sphere, and false otherwise
-constexpr bool rayHasHitSphere(ray::Point3 const &centre, double const radius,
-                               ray::Ray const &ray) noexcept {
-  // Get the vector from the ray origin to the sphere centre
-  auto const oc = ray.getOrigin() - centre;
-
-  // The equation of a ray is P(t) = A + tb, where P is a 3D position along a
-  // line, A is the ray origin, b is the ray direction, and t is a scalar
-  // value.
-  // We want to know if our ray ever hits the sphere anywhere. If it
-  // does, there is some t for which P(t) satisfies the sphere equation:
-  // (P - C) * (P - C) = r^2
-  // So we are looking for any t where (P(t)) - C * (P(t) - C) = r^2, or
-  // (A + tb - C) * (A + tb - C) = r^2
-  // If we expand this equation and move all the terms to the left-hand side,
-  // we get the following equation:
-  // (t^2b * b) + 2tb * (A - C) + (A - C) * (A - C) - r^2 = 0
-  // The vectors and r in this equation are all constant and known. The unknown
-  // is t, thus making this quadratic
-  // Recall the quadratic formula, x = (-b ± sqrt(b^2 - 4ac) / 2a).
-  // We now proceed to calculate our a, b, and c values.
-
-  auto const a = vec3::getDotProduct(ray.getDirection(), ray.getDirection());
-  auto const b = 2.0 * vec3::getDotProduct(oc, ray.getDirection());
-  auto const c = vec3::getDotProduct(oc, oc) - (radius * radius);
-
-  auto const discriminant = (b * b) - (4 * a * c);
-  return discriminant > 0;
-}
-
 /// \brief Produce a linear blend of white and blue colours
 /// \param[in] ray The ray whose colour is to be computed
 /// \returns A linear blend of white and blue colours
